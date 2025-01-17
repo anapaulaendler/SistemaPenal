f5ca3b891e1306995f1abc78714fcb39ac8fc008
unknown <anapaulaendler@gmail.com>
2025-01-17 16:45:38 -0300

    Services: WIP

--- Solution/src/PenalSystem.Domain/Services/BookService.cs
+++ Solution/src/PenalSystem.Domain/Services/BookService.cs
@@ -1,4 +1,5 @@
 using PenalSystem.Domain.DTOs;
+using PenalSystem.Domain.Entities;
 using PenalSystem.Domain.Interfaces;
 
 namespace PenalSystem.Domain.Services;
@@ -16,9 +17,40 @@ public class BookService : IBookService
         _uow = uow;
     }
 
-    public Task CreateBookActivityAsync(BookCreateDTO book)
+    public async Task CreateBookActivityAsync(BookCreateDTO book)
     {
-        throw new NotImplementedException();
+        await _uow.BeginTransactionAsync();
+
+        var prisoner = await _prisonerRepository.GetByIdAsync(book.PrisonerId);
+
+        var newBook = new Book()
+        {
+            PrisonerId = book.PrisonerId,
+            Isbn = book.Isbn,
+            Prisoner = prisoner,
+        };
+
+        await _bookRepository.AddAsync(newBook);
+        await _uow.CommitTransactionAsync();
+
+    // public virtual async Task<OperationResult<TCreateDTO>> AddAsync(TCreateDTO itemToCreate, CancellationToken cancellationToken = default(CancellationToken))
+    // {
+    //     TEntity entity = _mapper.Map<TEntity>(itemToCreate);
+    //     OperationResult operationResult = await OnAddingAsync(entity, cancellationToken);
+    //     if (!operationResult.IsValid)
+    //     {
+    //         return operationResult.Cast(itemToCreate);
+    //     }
+
+    //     await _repository.AddAsync(entity, cancellationToken);
+    //     OperationResult result = await _uow.SaveChangesAsync(cancellationToken);
+    //     if (result.IsValid)
+    //     {
+    //         await OnAddedAsync(new AddedEntityEventArgs<TEntity, object>(entity, itemToCreate), cancellationToken);
+    //     }
+
+    //     return result.Cast(itemToCreate);
+    // }
     }
 
     public Task<List<BookDTO>> GetBookActivitiesByPrisonerIdAsync(Guid prisonerId)
@@ -29,5 +61,17 @@ public class BookService : IBookService
     public Task<List<BookDTO>> GetBooksAsync()
     {
         throw new NotImplementedException();
+    }
+
+    public async Task ReducePrisonerPenalty(Guid prisonerId)
+    {
+        await _uow.BeginTransactionAsync();
+
+        Prisoner prisoner = await _prisonerRepository.GetByIdAsync(prisonerId);
+
+        prisoner.UpdatedReleaseDate.AddDays(-3);
+
+        await _prisonerRepository.Update(prisoner);
+        await _uow.CommitTransactionAsync();
     }
 }
